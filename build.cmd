8604cab1f69db2a8bc69c29a75a4e2ff1d3c5a34
fantaclaus <fantaclaus@users.noreply.github.com>
2014-05-16 03:28:42 +0400

    added logo

--- build.cmd
+++ build.cmd
@@ -6,11 +6,18 @@ rem if '%1'=='' echo no file name & goto err
 set tsc=tsc.cmd
 rem set tsc="C:\Program Files (x86)\Microsoft SDKs\TypeScript\0.9\tsc.exe"
 
-%tsc% src\d.ts\libcore.d.ts @files.txt --removeComments --noLib -d --out out\tswlib.js
+if not exist tmp md tmp
+call %tsc% src\d.ts\libcore.d.ts @files.txt --noLib  --removeComments -d --out tmp\tswlib.js
 
 if errorlevel 1 goto err
 
 rem TODO: remove ref to jquery in out\tswlib.d.ts
+
+if not exist out md out
+copy /b logo.txt + tmp\tswlib.js out\tswlib.js
+copy /b logo.txt + tmp\tswlib.d.ts out\tswlib.d.ts
+del tmp\*.* /q
+rmdir tmp
 
 goto ex
 
