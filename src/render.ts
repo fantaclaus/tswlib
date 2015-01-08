cb4ae6782d54cb6df6dd703e9871ead43e6ed106
fantaclaus <fantaclaus@users.noreply.github.com>
2015-01-08 06:15:11 +0300

    render.ts

--- src/render.ts
+++ src/render.ts
@@ -22,23 +22,38 @@ module tsw.render
 		}
 		public update()
 		{
-			var htm = RenderUtils.renderHtml(this._content);
+			var ctx = new CtxRootElement();
+			var htm = CtxScope.use(ctx, () => RenderUtils.renderHtml(this._content));
 			console.log('html: [%s]', htm);
 			this.htmlElement.innerHTML = htm;
 		}
 	}
 
-	class Ctx
+	export class Ctx
 	{
 		children: Ctx[] = [];
+
+		update(): void
+		{
+		}
 	}
 
-	class CtxElementChildren extends Ctx
+	class CtxRootElement extends Ctx
 	{
 
 	}
 
-	class CtxScope
+	class CtxElement extends Ctx
+	{
+
+	}
+
+	class CtxUpdatableChild extends Ctx
+	{
+		renderer: tsw.common.Renderer;
+	}
+
+	export class CtxScope
 	{
 		private static contexts: Ctx[] = [];
 
@@ -48,13 +63,13 @@ module tsw.render
 			return contexts.length == 0 ? null : contexts[contexts.length - 1];
 		}
 
-		static use(ctx: Ctx, action: () => void)
+		static use<T>(ctx: Ctx, action: () => T)
 		{
 			this.contexts.push(ctx);
 
 			try
 			{
-				action();
+				return action();
 			}
 			finally
 			{
@@ -70,33 +85,43 @@ module tsw.render
 			var items = [];
 			RenderUtils.addExpanded(items, content);
 
-			//console.log(items);
-
-			var htm: string;
-
-			CtxScope.use(new CtxElementChildren(), () =>
-			{
-				htm = tsw.utils.join(items, null, item => RenderUtils.renderItemHtml(item));
-			});
-
+			var htm = tsw.utils.join(items, null, item => RenderUtils.renderItem(item));
 			return htm;
 		}
-		private static renderItemHtml(item: any): string
+		private static renderItem(item: any): string
 		{
 			if (typeof item == 'boolean') return '';
 
-			if (item instanceof tsw.common.rawHtml) return (<tsw.common.rawHtml>item).value;
+			if (item instanceof tsw.common.rawHtml) return (<tsw.common.rawHtml> item).value;
 
 			if (item instanceof tsw.elements.elm)
 			{
-				var elm = <tsw.elements.elm> item;
-				var elmHtml = this.renderElement(elm);
-				return elmHtml;
+				return this.renderElement(<tsw.elements.elm> item);
+			}
+
+			var renderer = this.getRenderer(item);
+			if (renderer)
+			{
+				return this.renderUpdatableChild(renderer);
 			}
 
 			var s = item.toString();
-
 			return tsw.utils.htmlEncode(s);
+		}
+		private static renderUpdatableChild(renderer: tsw.common.Renderer): string
+		{
+			var ctx = new CtxUpdatableChild();
+			ctx.renderer = renderer;
+
+			var innerHtml = CtxScope.use(ctx, () =>
+			{
+				var content = renderer.render();
+				return RenderUtils.renderHtml(content);
+			});
+
+			var id = '1';
+			var marker = new HtmlBlockMarkers(id);
+			return "<!--" + marker.begin + "-->" + innerHtml + "<!--" + marker.end + "-->";
 		}
 		private static renderElement(elm: tsw.elements.elm): string
 		{
@@ -104,7 +129,9 @@ module tsw.render
 
 			var children = elm.z_getChildren();
 			//console.log('children: ', children);
-			var innerHtml = this.renderHtml(children);
+
+			var ctx = new CtxElement();
+			var innerHtml = CtxScope.use(ctx, () => RenderUtils.renderHtml(children));
 
 			var tagName = elm.z_getTagName();
 			if (!tagName) return innerHtml;
@@ -246,11 +273,21 @@ module tsw.render
 			return '"' + encoded + '"';
 		}
 
-		public static getRenderFn(renderer: any): tsw.common.RendererFn
+		public static getRenderer(item: any): tsw.common.Renderer
 		{
-			if (renderer instanceof Function) return renderer;
+			if (item instanceof Function)
+			{
+				var r = new Renderer();
+				r.render = item;
+				return r;
+			}
 
-			if (renderer.render) return renderer.render;
+			if (item.render instanceof Function)
+			{
+				var r = new Renderer();
+				r.render = item.render;
+				return r;
+			}
 
 			return null;
 		}
@@ -270,6 +307,29 @@ module tsw.render
 			{
 				target.push(v);
 			}
+		}
+	}
+
+	class Renderer implements tsw.common.Renderer
+	{
+		render: tsw.common.RendererFn;
+		beforeRemove: () => void;
+		afterInsert: () => void;
+	}
+
+	class HtmlBlockMarkers
+	{
+		begin: string;
+		end: string;
+
+		constructor(id: string)
+		{
+			this.begin = "BEGIN:" + id;
+			this.end = "END:" + id;
+		}
+		getHtml(innerHtml: string)
+		{
+			return "<!--" + this.begin + "-->" + innerHtml + "<!--" + this.end + "-->";
 		}
 	}
 }
