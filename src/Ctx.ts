49fecbbea7923b9dad611f4241c6f32ff0200d8c
fantaclaus <fantaclaus@users.noreply.github.com>
2016-08-28 03:04:43 +0300

    setContent, Renderer moved to Ctx.ts

--- src/Ctx.ts
+++ src/Ctx.ts
@@ -1,4 +1,20 @@
-﻿/**
+﻿namespace tsw
+{
+	export function setContent(htmlElement: HTMLElement, content: any): void
+	{
+		var ctxRoot = new tsw.internal.CtxRoot();
+		ctxRoot.render(content, htmlElement);
+	}
+
+	export interface Renderer
+	{
+		render: () => any;
+		afterAttach?: () => void;
+		beforeDetach?: () => void;
+	}
+}
+
+/**
  * @internal
  */
 namespace tsw.internal
--- src/common.ts
+++ src/common.ts
@@ -1,18 +1,3 @@
-namespace tsw
-{
-	export function setContent(htmlElement: HTMLElement, content: any): void
-	{
-		var ctxRoot = new tsw.internal.CtxRoot();
-		ctxRoot.render(content, htmlElement);
-	}
-	export interface Renderer
-	{
-		render: () => any;
-		afterAttach?: () => void;
-		beforeDetach?: () => void;
-	}
-}
-
 namespace tsw.ctxUtils
 {
 	export function attach(propKey: any): void
