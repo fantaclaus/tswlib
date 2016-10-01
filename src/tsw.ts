f2665e254da74d821699868bf1eac73111f3086d
fantaclaus <fantaclaus@users.noreply.github.com>
2016-10-01 20:24:28 +0300

    typo

--- src/tsw.ts
+++ src/tsw.ts
@@ -2,13 +2,13 @@ import * as html from './html';
 import * as ctxUtils from './CtxUtils2';
 import * as global from './PropDefs';
 import * as elements from "./htmlElements";
-import * as componenets from "./RadioGroup";
+import * as components from "./RadioGroup";
 
 export { html };
 export { ctxUtils };
 export { global };
 export { elements };
-export { componenets };
+export { components };
 
 export { setContent, Renderer } from './Ctx';
 export { Ref } from './Ref';
